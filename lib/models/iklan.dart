ea491380eec3fbb9cf96860e7a98289360bcccb6
Yukari <topexgames@yahoo.com>
2020-06-22 12:45:58 +0700

    iklan model

--- lib/models/iklan.dart
+++ lib/models/iklan.dart
@@ -3,25 +3,46 @@ import 'package:flutter/material.dart';
 class IklanModel {
   IklanModel({
     this.id,
+    this.idUser,
+    this.idShop,
     @required this.judul,
-    this.deskripsi,
+    @required this.deskripsi,
+    this.keyword,
+    this.tipe,
+    this.waktu,
+    this.waktuUpdate,
     @required this.lat,
     @required this.lng,
-    this.timee,
+    @required this.judulLapak,
+    @required this.pengiklan,
   });
 
   final int id;
+  final int idUser;
+  final int idShop;
   final String judul;
   final String deskripsi;
+  final String keyword;
+  final String tipe;
+  final DateTime waktu;
+  final DateTime waktuUpdate;
   final double lat;
   final double lng;
-  final int timee;
+  final String judulLapak;
+  final String pengiklan;
 
   IklanModel.fromJson(Map<String, dynamic> parsedJson)
-  : id = parsedJson['uid'],
-    judul = parsedJson['judul'],
-    deskripsi = parsedJson['deskripsi'],
-    lat = double.parse(parsedJson['lat']),
-    lng = double.parse(parsedJson['lng']),
-    timee = int.parse(parsedJson['timee']);
+  : id = int.parse(parsedJson['ID']),
+    idUser = int.parse(parsedJson['ID_USER']),
+    idShop = int.parse(parsedJson['ID_USER_LOCATION']),
+    judul = parsedJson['JUDUL'],
+    deskripsi = parsedJson['DESKRIPSI'],
+    keyword = parsedJson['KEYWORD'],
+    tipe = parsedJson['TIPE'],
+    waktu = DateTime.parse(parsedJson['TIMEE']),
+    waktuUpdate = DateTime.parse(parsedJson['LAST_UPDATED']),
+    lat = double.parse(parsedJson['LATITUDE']),
+    lng = double.parse(parsedJson['LONGITUDE']),
+    judulLapak = parsedJson['JUDUL_LAPAK'],
+    pengiklan = parsedJson['PENGIKLAN'];
 }
